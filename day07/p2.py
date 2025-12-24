63c45913c8628ca0deb69ab707707d3e55bdbb7d
justin <goldrabbit0926@gmail.com>
2025-12-24 17:19:27 +0900

    Finished Day 07

---
+++ day07/p2.py
@@ -0,0 +1,19 @@
+import sys
+
+
+def main():
+    data = open(sys.argv[1]).read()
+    positions = [int(x) for x in data.split(',')]
+
+    min_fuel = float('inf')
+    min_pos, max_pos = min(positions), max(positions)
+
+    for target in range(min_pos, max_pos + 1):
+        fuel = sum(abs(p - target) * (abs(p - target) + 1) // 2 for p in positions)
+        min_fuel = min(min_fuel, fuel)
+
+    print(min_fuel)
+
+
+if __name__ == '__main__':
+    main()
